7824323fd315fdfdee94cca20d3493472f8f9b37
Hemanta Kar <mr.hemanta@hotmail.com>
2023-01-15 23:42:30 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1", 
+{
+    "id": "title",
+    "key":"h1"
+},
+ "Nmaste React Course!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
